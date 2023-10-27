896f1f4e2128ddcc4d76280d322c95f419e3bad0
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-27 15:45:24 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom'
+import { Shop } from './Pages/shop/shop'
+import { cart } from './Pages/cart/cart'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Router>
+            <Routes>
+              <Route path='/' />
+
+              <Route path='/cart' />
+            </Routes>
+          </Router>
+        </BrowserRouter>
     </div>
   );
 }
