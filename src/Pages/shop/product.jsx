42f33b7057a940308e8361604b6819694105b799
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-28 11:45:18 -0400

    message

--- src/Pages/shop/product.jsx
+++ src/Pages/shop/product.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
 export const Product = (product) => {
     const { id, productName, price, productImage, category} = product.data
     return (
-    <div>
-        <h1>product</h1>
-    </div>
+    // <div className='product'>
+    //   <img src={productImage} />
+    //   <div className='description'>
+    //     <p><b>{productName}</b></p>
+    //     <p>{category}</p>
+    //     <p>${price}</p>
+    //   </div>
+    // </div>
+      <div className='product'>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={productImage} />
+        <Card.Body className='description'>
+          <Card.Title><b>{productName}</b></Card.Title>
+          <Card.Text >
+            This {productName} is part of the {category} category
+            Price: $ {price} 
+          </Card.Text>
+          <Button variant="primary">Buy me</Button>
+        </Card.Body>
+      </Card>
+      </div>
+      
   )
 }
 
