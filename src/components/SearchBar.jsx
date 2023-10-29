de002c0a749d340e8d6a213432ca1e76b6c68f3b
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-29 18:21:12 -0400

    message

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { MagnifyingGlass, X } from 'phosphor-react'
+import { MagnifyingGlass, X, XSquare } from 'phosphor-react'
 import { Link } from 'react-router-dom'
 
 import styles from './SearchBar.css'
@@ -7,26 +7,41 @@ import styles from './SearchBar.css'
 
 function SearchBar({ placeholder, data }) {
     const [filteredData, setFilteredData] = useState([])
+    const [wordEntered, setWordEntered] = useState('')
 
     const handleFilter= ( e ) => {
         const searchWord = e.target.value
+        setWordEntered(searchWord)
         const newFilter = data.filter((value) => {
-            return value.productName.includes(searchWord)
+            return value.productName.toLowerCase().includes(searchWord.toLowerCase())
         })
-        setFilteredData(newFilter)
+        if ( searchWord === "") {
+            setFilteredData([])
+        } else {
+            setFilteredData(newFilter)
+        }
+        
+    }
+
+    const clearInput = () => {
+        setFilteredData([])
+        setWordEntered('')
     }
 
   return (
     <div className='Search'>
         <div className='searchInputs'>
-            <input type='text' placeholder={placeholder} onChange={handleFilter}/>
+            <input type='text' placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
             <div className='searchIcon'>
-                <MagnifyingGlass size={22} color="#f90101" weight="bold" />
+                {filteredData.length === 0 ? <MagnifyingGlass size={22} color="#f90101" weight="bold" /> : (
+                    <XSquare size={22} color="#f90101" weight='bold' id="clearBtn" onClick={clearInput} />
+                )}
+                
             </div>
         </div>
         {filteredData.length != 0 && (
             <div className='dataResult'>
-            {filteredData.map((value, key) => {
+            {filteredData.slice(0, 5).map((value, key) => {
                 return <Link to="/shop" className='dataItem'> {value.productName} </Link>
             })}
         </div>
