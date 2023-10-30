d4fa556c848590f28ab2bda0f1afa1c0d9ddbc3a
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-10-29 20:49:13 -0400

    message

--- src/Pages/home/home.jsx
+++ src/Pages/home/home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import SubHeader from '../../components/SubHeader'
 import { Container } from 'reactstrap'
+import Lottie from "lottie-react";
+import phone from '../../assets/animation/phone.json'
+import styles from './home.css'
+
 
 export const Home = () => {
   return (
     <Container>
       <SubHeader current='' />
-      <div>
+      <div className='banner'>
         <h1>homet</h1>
+        <Lottie animationData={phone} style={{ width: 300, height: 300, }}/>
       </div>
     </Container>
   
