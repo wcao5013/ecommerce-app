82fdfeed67597a3a1c7c95472d425181f56522c5
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-11-01 15:44:12 -0400

    message

--- src/Pages/home/home.jsx
+++ src/Pages/home/home.jsx
@@ -15,6 +15,10 @@ export const Home = () => {
         <div className='centeredLottie'>
           <Lottie animationData={phone} style={{ width: 500, height: 500 }} />
         </div>
+        <div className='centertext'>
+          <p> 
+Explore a vibrant world of cutting-edge electronic products at our online store. Immerse yourself in a spectrum of colors, where technology meets style, and discover a range of electronic devices designed to enhance your lifestyle. From sleek smartphones in striking hues to eye-catching laptops and tablets, our ecommerce platform offers a diverse selection of color electronic products that cater to your individual preferences. Whether you're in search of a vivid smartphone that complements your personality or a vibrant smartwatch to add a pop of color to your daily routine, our curated collection ensures you find the perfect blend of functionality and aesthetics. Dive into our colorful array of electronic gadgets, where innovation meets design, and elevate your tech experience with a touch of brilliance. Shop now and infuse your life with the vibrancy of color and the power of advanced technology.</p>
+        </div>
       </div>
     </Container>
   
